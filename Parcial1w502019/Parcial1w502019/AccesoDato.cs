0b3e8495bfd71620e3033fcc3bd89913f28e0a36
JuanVisintini <63617599+JuanVisintini@users.noreply.github.com>
2020-07-02 10:51:25 -0300

    Parcial de mascotas

---
+++ Parcial1w502019/Parcial1w502019/AccesoDato.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Parcial1w502019
+{
+    class AccesoDato
+    {
+        OleDbCommand comando;
+        OleDbConnection conexion;
+        OleDbDataReader lector;
+        DataTable tabla;
+        string cadenaConexion;
+
+
+        public AccesoDato(string cadenaConexion)
+        {
+            comando = new OleDbCommand();
+            conexion = new OleDbConnection();
+            lector = null;
+            tabla = new DataTable();
+            this.cadenaConexion = cadenaConexion;
+        }
+        public AccesoDato()
+        {
+            comando = new OleDbCommand();
+            conexion = new OleDbConnection();
+            lector = null;
+            tabla = new DataTable();
+            cadenaConexion = null;
+        }
+        public OleDbCommand pComando
+        {
+            set { comando = value; }
+            get { return comando; }
+        }
+        public OleDbConnection pConexion
+        {
+            set { conexion = value; }
+            get { return conexion; }
+        }
+        public OleDbDataReader pLector
+        {
+            set { lector = value; }
+            get { return lector; }
+        }
+        public DataTable pTabla
+        {
+            set { tabla = value; }
+            get { return tabla; }
+        }
+        public string pCadenaconexion
+        {
+            set { cadenaConexion = value; }
+            get { return cadenaConexion; }
+        }
+        public void conectar()
+        {
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+            comando.Connection = conexion;
+            comando.CommandType = CommandType.Text;
+
+        }
+        public void desconectar()
+        {
+            conexion.Close();
+            conexion.Dispose();
+        }
+        public DataTable consultarTabla(string nombreTabla)
+        {
+            DataTable tabla = new DataTable();
+            conectar();
+            comando.CommandText = "SELECT * FROM " + nombreTabla;
+            tabla.Load(comando.ExecuteReader());
+            desconectar();
+            return tabla;
+        }
+        public void leerTabla(string nombreTabla)
+        {
+            conectar();
+            comando.CommandText = "SELECT * FROM " + nombreTabla;
+            lector = comando.ExecuteReader();    
+        }
+
+        public DataTable consutarBD(string consultaSQL)
+        {
+            DataTable table = new DataTable();
+            conectar();
+            comando.CommandText = consultaSQL;
+            tabla.Load(comando.ExecuteReader());
+            desconectar();
+            return tabla;
+
+        }
+        public  void actualizarBD(string consultaSQL)
+        {
+            conectar();
+            comando.CommandText = consultaSQL;
+            comando.ExecuteNonQuery();
+            desconectar();
+        }
+    }
+    
+}
---
+++ Parcial1w502019/Parcial1w502019/frmMascota.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Parcial1w502019
+{
+    public partial class frmMascota : Form
+    {
+        AccesoDato oDato = new AccesoDato(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Usuario\Desktop\Parcial1w502019\dbfMascotas.mdb");
+        const int tam = 5;
+        Mascota[] am = new Mascota[tam];
+        int c; 
+
+
+        public frmMascota()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMascota_Load(object sender, EventArgs e)
+        {
+            cargarCombo(cboEspecie, "Especie");
+            cargarLista("Mascota");
+
+        }
+        private void cargarCombo(ComboBox combo, string nombreTabla)
+        {
+            DataTable dt = new DataTable();
+            dt = oDato.consultarTabla(nombreTabla);
+            combo.DataSource = dt;
+            combo.ValueMember = dt.Columns[0].ColumnName;
+            combo.DisplayMember = dt.Columns[1].ColumnName;
+            combo.DropDownStyle = ComboBoxStyle.DropDownList;
+
+        }
+        private void cargarLista(string nombreTabla)
+        {
+            c = 0;
+            lstMascotas.Items.Clear();
+            oDato.leerTabla(nombreTabla);
+            while (oDato.pLector.Read())
+            {
+                Mascota m = new Mascota();
+                if (!oDato.pLector.IsDBNull(0))
+                {
+                    am[c].pCodigo = oDato.pLector.GetInt32(0);
+
+                }
+                    
+
+                if (!oDato.pLector.IsDBNull(1))
+                {
+                    am[c].pNombre = oDato.pLector.GetString(1);
+                }
+                if (!oDato.pLector.IsDBNull(2))
+                {
+                    am[c].pEspecie = oDato.pLector.GetInt32(2);
+                }
+                if (!oDato.pLector.IsDBNull(3))
+                {
+                    am[c].pSexo = oDato.pLector.GetInt32(3);
+                }
+                if (!oDato.pLector.IsDBNull(4))
+                {
+                    am[c].pFechaNacimiento = oDato.pLector.GetDateTime(4);            
+                }
+                am[c] = m;
+                c++;
+            }
+          
+        }
+       
+
+       
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        { 
+            
+      
+          
+        }
+
+        private void lstMascotas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
